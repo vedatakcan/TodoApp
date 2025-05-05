1dd07610f2f3bb17a677b6443d8a63341e8f7b7c
Vedat Akcan <akcanvedat47@gmail.com>
2025-05-06 01:49:41 +0300

    Migration 1 den 2 ye geçiş için gerekli işlemler eklendi. Veritabanı versiyonu 2 ye yükseltildi.

--- app/src/main/java/com/vedatakcan/todoapp/data/database/TodoDatabase.kt
+++ app/src/main/java/com/vedatakcan/todoapp/data/database/TodoDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.vedatakcan.todoapp.data.model.Todo
 
-@Database(entities = [Todo::class], version = 1)
+@Database(entities = [Todo::class], version = 2)
 abstract class TodoDatabase : RoomDatabase() {
     abstract fun todoDao(): TodoDao
 }
--- app/src/main/java/com/vedatakcan/todoapp/di/TodoModule.kt
+++ app/src/main/java/com/vedatakcan/todoapp/di/TodoModule.kt
@@ -1,8 +1,9 @@
 package com.vedatakcan.todoapp.di
 
 import android.app.Application
-import androidx.compose.ui.graphics.findFirstCubicRoot
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.vedatakcan.todoapp.data.database.TodoDatabase
 import com.vedatakcan.todoapp.data.repository.TodoRepository
 import dagger.Module
@@ -15,6 +16,14 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object TodoModule {
 
+    // Migration 1'den 2'ye geçiş için tanımlama
+    private val MIGRATION_1_2 = object : Migration(1, 2) {
+        override fun migrate(db: SupportSQLiteDatabase) {
+            // Örneğin, yeni bir sütun ekliyoruz
+            db.execSQL("ALTER TABLE todos ADD COLUMN createdAt INTEGER DEFAULT 0 NOT NULL")
+        }
+    }
+
     @Provides
     @Singleton
     fun provideTodoDatabase(application: Application): TodoDatabase {
@@ -22,7 +31,10 @@ object TodoModule {
             application,
             TodoDatabase::class.java,
             "todos"
-        ).build()
+        )
+            // Migration'ı burada ekliyoruz
+            .addMigrations(MIGRATION_1_2) // Veritabanı sürümü 1'den 2'ye geçiş
+            .build()
     }
 
     @Provides
