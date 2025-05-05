5b6184a9bd328ade4b48de1e24a320a843f67340
Vedat Akcan <akcanvedat47@gmail.com>
2025-05-06 00:25:46 +0300

    Cart yüksekliği sınırlandırıldı.

--- app/src/main/java/com/vedatakcan/todoapp/ui/view/TodoListScreen.kt
+++ app/src/main/java/com/vedatakcan/todoapp/ui/view/TodoListScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.vedatakcan.todoapp.data.model.Todo
@@ -93,7 +94,9 @@ fun TodoItem(
         ) {
             Text(
                 text = if (todo.isDone) "✅ ${todo.title}" else todo.title,
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.weight(1f),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
             IconButton(onClick = { onToggleDone(todo) }) {
                 Icon(Icons.Default.Check, contentDescription = "Tamamlandı")
